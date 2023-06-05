b1e3b6b15780715558d2b35feb5faa158c2ca494
N Jannasch <NJannasch@users.noreply.github.com>
2023-06-05 11:41:01 +0200

    Update install.sh

--- kubesphere/install.sh
+++ kubesphere/install.sh
@@ -1,9 +1,13 @@
-kubectl apply -f https://github.com/kubesphere/ks-installer/releases/download/v3.3.1/kubesphere-installer.yaml
-
-kubectl delete -f https://github.com/kubesphere/ks-installer/releases/download/v3.3.1/cluster-configuration.yaml
-
-
-
-
 curl -L -O https://github.com/kubesphere/ks-installer/releases/download/v3.3.1/cluster-configuration.yaml
 curl -L -O https://github.com/kubesphere/ks-installer/releases/download/v3.3.1/kubesphere-installer.yaml
+
+kubectl apply -f kubesphere-installer.yaml
+kubectl apply -f cluster-configuration.yaml
+
+# kubectl delete -f kubesphere-installer.yaml
+# kubectl delete -f cluster-configuration.yaml
+
+
+curl -L -O https://raw.githubusercontent.com/kubesphere/ks-installer/release-3.1/scripts/kubesphere-delete.sh
+chmod +x kubesphere-delete.sh
+# ./kubesphere-delete.sh
